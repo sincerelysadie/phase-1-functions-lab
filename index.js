755c8dc66850d7464be7ba6a0812009a6bee7880
Sadie <sadie@Sadies-MacBook-Pro.local>
2022-03-30 16:14:41 -0700

    commit tree ed54aeec9c8207077e6df700b75787b2d372e2e1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    console.log(someValue);
+    if (someValue > 44) {
+        distanceFromHqInBlocks + 8;
+    } else if (someValue < 44) {
+        distanceFromHqInBlocks - 8;
+    }
+    return someValue - 42;  
+}
+//third test is getting -8, and it wants +8
+//if someValue < 42 than we want to change equation so it return + number
